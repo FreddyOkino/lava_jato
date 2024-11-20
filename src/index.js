08a0d142332121f511a0626ae094f99969298529
FreddyOkino <freddyokino@hotmail.com>
2024-11-20 17:46:26 -0400

    feito

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const prisma = require("@prisma/client").PrismaClient;
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const options = {
   definition: {
